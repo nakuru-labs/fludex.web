49685c2d4433d582bbe24fbb9b062ada75cdd883
Serhii Smirnov <d.duck.dev@gmail.com>
2026-03-26 10:30:58 +0100

    update bg

--- docs/.vitepress/theme/components/BokehBackground.vue
+++ docs/.vitepress/theme/components/BokehBackground.vue
@@ -25,51 +25,51 @@
 
 /* top-left large blob */
 .bokeh__blob--1 {
-  width: 580px;
-  height: 460px;
-  top: -8%;
-  left: -10%;
-  background: rgba(85, 42, 128, 0.30);
+  width: 620px;
+  height: 500px;
+  top: -10%;
+  left: -8%;
+  background: rgba(90, 45, 140, 0.50);
   filter: blur(90px);
 }
 
 /* upper-center blob */
 .bokeh__blob--2 {
-  width: 420px;
-  height: 340px;
-  top: 5%;
-  left: 22%;
-  background: rgba(75, 38, 118, 0.20);
+  width: 480px;
+  height: 380px;
+  top: 8%;
+  left: 20%;
+  background: rgba(80, 40, 125, 0.42);
   filter: blur(100px);
 }
 
 /* mid-left blob */
 .bokeh__blob--3 {
-  width: 360px;
-  height: 300px;
-  top: 38%;
-  left: -5%;
-  background: rgba(72, 36, 110, 0.22);
+  width: 420px;
+  height: 340px;
+  top: 40%;
+  left: -6%;
+  background: rgba(78, 38, 120, 0.44);
   filter: blur(85px);
 }
 
 /* center diagonal blob */
 .bokeh__blob--4 {
-  width: 400px;
-  height: 300px;
-  top: 45%;
-  left: 20%;
-  background: rgba(68, 34, 105, 0.16);
+  width: 460px;
+  height: 340px;
+  top: 48%;
+  left: 18%;
+  background: rgba(72, 36, 112, 0.35);
   filter: blur(100px);
 }
 
 /* bottom-left blob */
 .bokeh__blob--5 {
-  width: 340px;
-  height: 260px;
-  top: 72%;
-  left: 5%;
-  background: rgba(65, 32, 100, 0.20);
+  width: 380px;
+  height: 300px;
+  top: 74%;
+  left: 4%;
+  background: rgba(75, 38, 115, 0.40);
   filter: blur(80px);
 }
 </style>
